152937b5bc88960253f30723f4e408b305da631f
rahul <rahulpal2109@gmail.com>
2022-01-30 21:45:50 +0530

    Submission 29Jan

---
+++ GeeksForGeeks/array/coins_of_geekland/CoinsOfGeekland.java
@@ -0,0 +1,13 @@
+package GeeksForGeeks.array.coins_of_geekland;
+
+/*
+    Coins of Geekland
+
+    https://practice.geeksforgeeks.org/problems/257a9e27fb3e58255622c8dcb06e0919cc1c6c11/1
+    Medium
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+ */
+public class CoinsOfGeekland {
+}
