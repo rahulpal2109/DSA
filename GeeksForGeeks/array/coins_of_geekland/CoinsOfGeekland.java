77f610150a86d2d8bd8623f1f725d7a65f302c1a
rahul <rahulpal2109@gmail.com>
2022-01-31 23:54:49 +0530

    Submission 29Jan

--- GeeksForGeeks/array/coins_of_geekland/CoinsOfGeekland.java
+++ GeeksForGeeks/array/coins_of_geekland/CoinsOfGeekland.java
@@ -101,6 +101,7 @@ public class CoinsOfGeekland {
                 {5, 5, 5, 5, 5}};
         int[][] arr2 = {{4}};
 
+        // print 2D array
         /*for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] + " ");
