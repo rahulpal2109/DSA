ff0be49dcd1c48380caf40a97a7e9ae821a540a7
rahul <rahulpal2109@gmail.com>
2022-02-02 22:10:43 +0530

    Submission Feb 2

---
+++ leetcode/string/_438_find_all_anagrams_in_a_string/FindAllAnagramsInAString.java
@@ -0,0 +1,53 @@
+package leetcode.string._438_find_all_anagrams_in_a_string;
+
+import java.util.*;
+
+public class FindAllAnagramsInAString {
+    private static List<Integer> findAnagrams(String s, String t) {
+        List<Integer> result = new LinkedList<>();
+        if(t.length()> s.length()) return result;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int counter = map.size();
+
+        int begin = 0, end = 0;
+        int head = 0;
+        int len = Integer.MAX_VALUE;
+
+
+        while(end < s.length()){
+            char c = s.charAt(end);
+            if( map.containsKey(c) ){
+                map.put(c, map.get(c)-1);
+                if(map.get(c) == 0) counter--;
+            }
+            end++;
+
+            while(counter == 0){
+                char tempc = s.charAt(begin);
+                if(map.containsKey(tempc)){
+                    map.put(tempc, map.get(tempc) + 1);
+                    if(map.get(tempc) > 0){
+                        counter++;
+                    }
+                }
+                if(end-begin == t.length()){
+                    result.add(begin);
+                }
+                begin++;
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List result1 = Arrays.asList(0, 6);
+        assert findAnagrams("cbaebabacd", "abc").equals(result1);
+
+        List result2 = Arrays.asList(0, 1, 2);
+        assert findAnagrams("abab", "ab").equals(result2);
+    }
+}
